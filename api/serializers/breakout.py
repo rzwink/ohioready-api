3bc5caf33c3f7ab8959a26c75bbc8b699855f121
Rob Z <rzwink@gmail.com>
2020-03-24 10:23:37 -0400

    add breakout serializer

--- api/serializers/breakout.py
+++ api/serializers/breakout.py
@@ -4,6 +4,9 @@ from api.models import Breakout
 
 
 class BreakoutSerializer(serializers.HyperlinkedModelSerializer):
+    county = serializers.CharField(source="county.name", read_only=True)
+    source = serializers.CharField(source="source.name", read_only=True)
+
     class Meta:
         model = Breakout
         fields = [
