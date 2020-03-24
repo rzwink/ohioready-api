6c668e848fc396b0607fb8f8171f5b21a24ea231
Rob Z <rzwink@gmail.com>
2020-03-24 16:55:01 -0400

    updated import csv

--- api/admin/case.py
+++ api/admin/case.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
+from import_export import fields
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
 from api.models import Case
+from api.models import County
 
 
 class CaseResource(resources.ModelResource):
+    county = fields.Field(
+        column_name="county",
+        attribute="county",
+        widget=ForeignKeyWidget(County, "name"),
+    )
+
     class Meta:
         model = Case
-        fields = ("id", "as_of", "county__name", "total", "deaths", "recovered")
+        skip_unchanged = True
+        fields = ("id", "as_of", "county", "total", "deaths", "recovered", "county_id")
+        export_order = (
+            "id",
+            "as_of",
+            "county",
+            "total",
+            "deaths",
+            "recovered",
+        )
 
 
 class CaseAdmin(ImportExportModelAdmin):
