194ff2642c7ebc938ef8521cd3d8b664280f009d
Rob Z <rzwink@gmail.com>
2020-03-25 15:16:39 -0400

    add include serializer

--- api/serializers/event.py
+++ api/serializers/event.py
@@ -4,9 +4,14 @@ from api.models import Event
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    included_serializers = {
+        "authorizer": "api.serializers.AuthorizerSerializer",
+        "tags": "api.serializers.TagSerializer",
+        "article": "api.serializers.ArticleSerializer",
+    }
+
     class Meta:
         model = Event
-
         fields = [
             "published_on",
             "scope",
